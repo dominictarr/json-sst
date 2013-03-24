5e63cb8f174a284c8d932053d6fffe129e1e9738
Dominic Tarr <dominic.tarr@gmail.com>
2013-03-24 08:41:43 +0000

    tidy

--- block-iterator.js
+++ block-iterator.js
@@ -6,7 +6,7 @@ var fs = require('fs')
 var pull = require('pull-stream')
 
 var Iterator =
-pull.pipeableSource(function (next, close) {
+pull.pipeableSource(function (read, close) {
   var i = 0
   return function (end, cb) {
     if(end)
@@ -14,7 +14,7 @@ pull.pipeableSource(function (next, close) {
         cb(err || true)
       }) : cb && cb(end)
     else
-      next(i++, function (err, data) {
+      read(i++, function (err, data) {
         process.nextTick(function () {
           cb(err || data == null, data) //means end in an iterator.
         })
@@ -26,18 +26,24 @@ var shasum = require('shasum')
 
 function calcBlock(stat, opts, i) {
   opts = opts || {}
+  if(opts.offset)
+    i = i + opts.offset
+
   var blocks = Math.floor(stat.size / stat.blksize)
   var j = opts.reverse ? blocks - i : i
   var start = j * stat.blksize
   var limit = opts.limit ? opts.limit - 1 : blocks
   if(i > Math.min(blocks, limit)) return false
   return {
-    length: (j + 1) * stat.blksize > stat.size
+    length: (
+      (j + 1) * stat.blksize > stat.size
       ? stat.size - j * stat.blksize
-      : stat.blksize,
-    position: start, size: stat.size,
-    i: j,
-    end: i > blocks
+      : stat.blksize
+    ),
+    position: start,
+    size    : stat.size,
+    i       : j,
+    end     : i > blocks
   }
 }
 
@@ -45,19 +51,16 @@ var blockIterator = module.exports = function (stat, opts) {
   opts = opts || {}
   var blocks = Math.floor(stat.size / stat.blksize)
 
-  var initial = opts.inital
-
   return Iterator(function (i, cb) {
     var range = calcBlock(stat, opts, i)
+
     if(!range) return cb(null, null)
     var block = new Buffer(range.length)
     fs.read(stat.fd, block, 0, range.length, range.position, function (err) {
-      console.log('block')
       cb(err, block.toString())
     })
   })
 }
-
 
 module.exports.calcBlock = calcBlock
 
