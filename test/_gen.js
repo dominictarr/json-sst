f3b3fdb7bc3d641c2cf912ea1b6099ac8055e77e
Dominic Tarr <dominic.tarr@gmail.com>
2013-05-17 16:24:40 +0200

    port to use pull-streams

--- test/_gen.js
+++ test/_gen.js
@@ -1,10 +1,10 @@
 var randomName = require('random-name')
-var Iterator   = require('async-iterator')
 var shasum     = require('shasum')
 var test       = require('tape')
 var assert     = require('assert')
 var fs         = require('fs')
 var opts       = require('optimist').argv
+var pull       = require('pull-stream')
 
 try {
   fs.unlinkSync('/tmp/whatever-test.json.sst')
@@ -21,50 +21,45 @@ array.sort(function (a, b) {
   return a.key < b.key ? -1 : a.key > b.key ? 1 : 0 
 })
 
-var it = Iterator(function (i, cb) {
-  cb(null, array[i])
-})
+var it = pull.values(array)
 
 var SST = require('../')
-var dbDir = '/tmp/whatever-test.json.sst'
-SST.createSST(dbDir, it, function (err) {
+var sstFile = '/tmp/whatever-test.json.sst'
+SST.createSST(sstFile, it, function (err) {
+  console.log('create')
+  var sst = SST(sstFile)
 
-  SST(dbDir, function (err, sst) {
-
+  sst.open(function (err) {
+    console.log('SST')
     test('forward', function (t) {
       var it = sst.iterator()
-      t.plan(200)
+      t.plan(opts.n || 200)
       var i = 0
 
-      ;(function next () {
-        it.next(function (err, item) {
-          if(item == null)
-            return t.end()
-
-          t.deepEqual(item, array[i++])
-          next()
-        })
-      })();
+      it
+      .pipe(pull.through(console.log))
+      .pipe(pull.drain(function (item) {
+        t.deepEqual(item, array[i++])
+      }, function () {
+        return t.end()
+      }))
 
     })
 
     test('reverse', function (t) {
       var it = sst.iterator({reverse: true})
-      t.plan(200)
+
+      t.plan(opts.n || 200)
 
       var i = array.length
 
-      ;(function next () {
-        it.next(function (err, item) {
-          if(item == null)
-            return t.end()
+      it.pipe(pull.drain(function (item) {
           --i
-
           t.deepEqual(item, array[i])
           assert.deepEqual(item, array[i])
-          next()
-        })
-      })();
+      }, function () {
+        return t.end()
+      }))
 
     })
   })
